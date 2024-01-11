9180dc2b069222c4fd417c5d810960ea6c1ebc68
rafsanrjkhan <rjkhan104@gmail.com>
2024-01-11 15:56:35 +0600

    testimonial and reviews update

--- resources/views/components/frontend/layouts/elements/testimonial-section.blade.php
+++ resources/views/components/frontend/layouts/elements/testimonial-section.blade.php
@@ -5,8 +5,8 @@
                 <div class="section-title text-center">
                     <h6>Testimonial</h6>
                     <h1>NSS Client <span>Testimonial</span></h1>
-                    <p>Continually optimize cross functional applications via client-centric paradigms.
-                    Intrinsicly incubate 2.0 internal or "organic" sources whereas.</p>
+                    <p>NSS will give you the best services and we continuously improves cross-functional 
+                        applications by employing changes that are client-centric.</p>
                 </div>
             </div>
         </div>
@@ -17,15 +17,17 @@
                         <img src="{{ asset('ui/frontend/assets')}}/images/testi-1.png" alt="">
                     </div>
                     <div class="testi-content">
-                        <h2>Anjelina Kemi</h2>
-                        <span>UI/UX Designer</span>
+                        <h2>Suraia Shanta</h2>
+                        <span>SQA Engineer(PONDIT)</span>
                     </div>
                 </div>
-                <p>Synergistically formulate accurate tailer whereas collaboration 
-                and idea sharing. Enthusiastically build diverse action items where is
-                equity invested products. Dramatically.	</p>
+                <p>I am delighted to share my experience with NSS, my virtual school for Software Quality 
+                    Assurance (SQA). Enrolling in NSS has been a transformative journey, equipping me with 
+                    comprehensive knowledge and practical skills in SQA. The curriculum & learning 
+                    environment fosters interactive discussions, and the instructors are industry experts 
+                    who provide valuable insights. NSS has played a pivotal role in shaping my career in SQA, 
+                    and I highly recommend it to aspiring SQA professionals.</p>
                 <div class="testi-rating">
-                    <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -42,13 +44,15 @@
                         <img src="{{ asset('ui/frontend/assets')}}/images/testi-2.png" alt="">
                     </div>
                     <div class="testi-content">
-                        <h2>Anjelina Kemi</h2>
-                        <span>UI/UX Designer</span>
+                        <h2>Asif Shazid</h2>
+                        <span>React Developer (NSS)</span>
                     </div>
                 </div>
-                <p>Synergistically formulate accurate tailer whereas collaboration 
-                and idea sharing. Enthusiastically build diverse action items where is
-                equity invested products. Dramatically.	</p>
+                <p>
+                    Being a React Developer at NSS for two years has been immensely rewarding. NSS is an 
+                    innovative tech company that values talent and fosters growth. The collaborative work 
+                    culture, challenging projects, and supportive management make it a fulfilling workplace. 
+                    Proud to be part of NSS's dynamic community and excited for future opportunities.</p>
                 <div class="testi-rating">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -66,13 +70,15 @@
                         <img src="{{ asset('ui/frontend/assets')}}/images/testi-1.png" alt="">
                     </div>
                     <div class="testi-content">
-                        <h2>Anjelina Kemi</h2>
-                        <span>UI/UX Designer</span>
+                        <h2>Suraiya Paiker</h2>
+                        <span>CEO (SP Marketing Club)</span>
                     </div>
                 </div>
-                <p>Synergistically formulate accurate tailer whereas collaboration 
-                and idea sharing. Enthusiastically build diverse action items where is
-                equity invested products. Dramatically.	</p>
+                <p>I used to spend hours sending personalized emails and managing social media posts. 
+                    Since using Marketing Cloud Website which was build by NSS, I've automated most of my 
+                    marketing tasks, freeing up my time to focus on growing my business. My email open rates 
+                    have doubled, and I've generated more leads than ever before. NSS is a game-changer for 
+                    small businesses like mine.</p>
                 <div class="testi-rating">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -90,13 +96,14 @@
                         <img src="{{ asset('ui/frontend/assets')}}/images/testi-2.png" alt="">
                     </div>
                     <div class="testi-content">
-                        <h2>Anjelina Kemi</h2>
-                        <span>UI/UX Designer</span>
+                        <h2>Rafsan J.K. Ananta</h2>
+                        <span>MD (Gadget Tech BD)</span>
                     </div>
                 </div>
-                <p>Synergistically formulate accurate tailer whereas collaboration 
-                and idea sharing. Enthusiastically build diverse action items where is
-                equity invested products. Dramatically.	</p>
+                <p>NSS exceeded expectations in crafting our e-commerce site, Gadget Tech BD. Their expertise 
+                    and attention to detail resulted in a sleek, user-friendly platform. The team's 
+                    professionalism and commitment made the development process seamless. Highly recommend
+                    NSS for top-notch websitesolutions.	</p>
                 <div class="testi-rating">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -114,13 +121,13 @@
                         <img src="{{ asset('ui/frontend/assets')}}/images/testi-1.png" alt="">
                     </div>
                     <div class="testi-content">
-                        <h2>Anjelina Kemi</h2>
-                        <span>UI/UX Designer</span>
+                        <h2>Anila Tabassum</h2>
+                        <span>Chief Repoter (NewsBD)</span>
                     </div>
                 </div>
-                <p>Synergistically formulate accurate tailer whereas collaboration 
-                and idea sharing. Enthusiastically build diverse action items where is
-                equity invested products. Dramatically.	</p>
+                <p>NewsBD, our dream news site, went from pixels to primetime thanks to NSS. Their nimble team crafted a 
+                    platform as sleek as my headlines, intuitive for me to manage, and built to scale with my ambitions. 
+                    Now, NewsBD thrives, a testament to NSS's tech magic and people's focus. Highly recommend! 	</p>
                 <div class="testi-rating">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
